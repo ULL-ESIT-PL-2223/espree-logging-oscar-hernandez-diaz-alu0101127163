108f33f4169027bd18f9029e48ecb6470ac55eae
alu0101127163 <alu0101127163@ull.edu.es>
2023-03-02 11:00:49 +0000

    Pruebas

--- src/logging-espree.js
+++ src/logging-espree.js
@@ -2,15 +2,73 @@ import * as escodegen from "escodegen";
 import * as espree from "espree";
 import * as estraverse from "estraverse";
 import * as fs from "fs/promises";
+import * as esprima from "esprima";
 
-export async function transpile(inputFile, outputFile) {
-  // Fill in the code here
+
+/**
+ * Read the file with the js program, calls addLogin to add the login messages and writes the output
+ * @param {string} input_file - The name of the input file
+ * @param {string} output_file - The name of the output file
+ */
+export async function transpile(input_file, output_file) {
+
+  try {
+     let program = await fs.readFile(input_file, 'utf8');
+     let output = addLogging(program);
+     if (output_file === undefined) {
+      console.log(output);
+      return;
+     }
+     await fs.writeFile(output_file, output);
+   }
+   catch (err) {
+     console.log(err);
+   }
 }
 
+/** 
+ * Builds the AST and
+ * Traverses it searching for function nodes and callas addBeforeNode to transform the AST
+ * @param {string} code -the source code 
+ * @returns -- The transformed AST 
+ */
 export function addLogging(code) {
-  // Fill in the code here
+  const ast = esprima.parse(code, { ecmaVersion: esprima.latestEcmaVersion, loc: true });
+  estraverse.traverse(ast, {
+    enter: function (node, parent) {
+      if (node.type === 'FunctionDeclaration' ||
+        node.type === 'FunctionExpression' ||
+        node.type === 'ArrowFunctionExpression') {
+        addBeforeCode(node);
+      }
+    }
+  });
+  return escodegen.generate(ast);
 }
 
-function addBeforeCode(node) {
- // Fill in the code here
+/**
+ * AST transformation
+ * @param {AST function type node} node 
+ */
+export function addBeforeCode(node) {
+  const name = node.id ? node.id.name : '<anonymous function>';
+  const parameters = node.params.map(param => `\$\{${param.name}\}`);
+  const beforeCode = `console.log(\`Entering ${name}(${parameters}) at line ${node.loc.start.line}\`);`;
+  const newbeforeCode = spacesAfterComas(beforeCode);
+  const beforeNodes = espree.parse(newbeforeCode, { ecmaVersion: 6 }).body;
+  node.body.body = beforeNodes.concat(node.body.body);
+}
+
+export function spacesAfterComas (text)
+{
+  let newBeforeCode = "";
+  for (let i = 0; i < text.length; i++) {
+      if (text[i] == ',') {
+          newBeforeCode += ', ';
+      }
+      else {
+          newBeforeCode += text[i];
+      }
+  }
+  return newBeforeCode;
 }
